1cd8e2b86c71490b145e6a5762d0e961edc4d487
nullish-cat <nullishcat@fmhy.net>
2023-10-23 18:26:59 -0400

    Add disc ripping guide

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import svelte from "@astrojs/svelte";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nullish.cat',
-  integrations: [mdx(), sitemap(), svelte()]
+  integrations: [mdx(), sitemap(), svelte(), react()]
 });
